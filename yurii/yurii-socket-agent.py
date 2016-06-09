9a055e8652bec9bd871667981894e2cf2f6cc17b
yurii-andrieiev <yurii.andrieiev@gmail.com>
2016-06-09 08:50:12 +0300

    Keep track of statespace

--- yurii/yurii-socket-agent.py
+++ yurii/yurii-socket-agent.py
@@ -3,11 +3,13 @@ import socket
 from pybrain.rl.learners.valuebased.interface import ActionValueInterface
 from pybrain.rl.environments import Environment
 from scipy import *
-
+import json
+import os.path
 
 RESET_CMD = str([0, 0, 0, 1])
 DEFAULT_IP = '127.0.0.1'
 DEFAULT_PORT = 50007
+state_space_filename = "state_space.json"
 
 actions = [[0, 0, 0, 0], [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [1, 1, 0, 0], [0, 1, 1, 0], [1, 0, 1, 0],
            [1, 1, 1, 0]]
@@ -57,7 +59,6 @@ class DefaultController(ActionValueInterface):
             new = 1
         else:
             new = 0
-        # keys map [up, left, right, new]
         return up, left, right, new
 
     def getActionValues(self, state):
@@ -67,65 +68,28 @@ class DefaultController(ActionValueInterface):
 environment = VerticalLandingEnvironment()
 controller = DefaultController()
 
-while True:
-    sensors = environment.getSensors()
-    if not sensors:
-        break
-    action = controller.getMaxAction(sensors)
-    environment.performAction(action)
+state_space = list()
 
-    agent_state, platform_state, system_state = sensors
-    print agent_state, platform_state, system_state
-    print "{" + str(agent_state) + "," + str(platform_state) + "," + str(system_state) + "}" + ","
-    # output example
-    # {'dist': 30.396299362182617, 'contact_time': 0, 'vx': -21.335142135620117, 'vy': 0.9811121821403503,
-    # 'angle': -0.05268074572086334, 'px': 43.301612854003906, 'py': 42.239990234375, 'live': True, 'contact': False,
-    # 'fuel': 791.9, 'type': 'actor', 'enj': True, 'wind': 32.0}
-    #
-    # {'angle': 0.01994907110929489, 'px': 52.044925689697266, 'py': 4.305685997009277, 'vx': 0.8977082371711731,
-    # 'vy': 0.7780137658119202, 'type': 'decoration'}
-    #
-    # {'step': 99, 'type': 'system', 'flight_status': 'none', 'action': [0, 0, 1, 0], 'is_terminal_state': False,
-    # 'score': 25.16671304950599}
-    #
-    #
-    # 'dist' - distance between two nearest points of rocket and a platform body
-    #
-    # 'contact_time' - time counter, which starts from the moment when rocket contacts with the platform
-    #                  you need to keep contact with the platform for some time, without destroying yourself, to win
-    #
-    # 'vx' | 'vy' - vertical and horizontal velocity
-    #
-    # 'angle' - angle of hade
-    #
-    # 'px' | 'py' - coordinates of the body central point
-    #
-    # 'live' - still alive? "False" if not
-    #
-    # 'contact' - is there a contact with the platform? "False" if not
-    #
-    # 'fuel' - the amount of remaining fuel
-    #
-    # 'type' - type of the object | "actor" - rocket | "decoration" - platform | "system" - world
-    #
-    # 'enj' - are the engines workable? "False" if fuel tank is empty
-    #
-    # 'wind' - wind strength
-    #
-    # 'step' - iteration counter
-    #
-    # 'flight_status' - win or loss? | "landed" or "destroyed"?
-    #                   "none" means that you don't know, whether you landed or destroyed, or maybe still flying
-    #                   "landed" means that you won
-    #
-    # 'action' - action performed in the step
-    #
-    # 'is_terminal_state' - means that the system has reached it's terminal state: rocket landed or destroyed
-    #                       or drowned etc.
-    #                       Difference with 'flight_status' is in the point of view, 'flight_status' describes only the
-    #                       rocket status but not whole system.
-    #
-    # 'score' - synthetic score, you can use it or write your own
+try:
+    while True:
+        sensors = environment.getSensors()
+        if not sensors:
+            break
+        action = controller.getMaxAction(sensors)
+        environment.performAction(action)
+
+        agent_state, platform_state, system_state = sensors
+        state_space.append(agent_state)
+
+        print agent_state, platform_state, system_state
+        print "{" + str(agent_state) + "," + str(platform_state) + "," + str(system_state) + "}" + ","
+
+except Exception:
+    with open(state_space_filename, "w+") as text_file:
+        state_space_as_json = json.dumps(state_space, indent=4, separators=(',', ': '))
+        text_file.write(state_space_as_json)
+        text_file.read()
+    raise
 
 environment.socket.close()
 print "Socket closed"
